314f5328d6dc1a4233a988a856d5af52460ec352
gonggaobushang <47070965+gonggaobushang@users.noreply.github.com>
2019-10-30 14:23:19 +0800

    Update jiebaR.R

--- jiebaR.R
+++ jiebaR.R
@@ -1,4 +1,3 @@
-# https://mp.weixin.qq.com/s/H6ZKEZhxBprQk8qinIrPww
 library(jiebaRD)
 library(jiebaR)
 
@@ -16,14 +15,11 @@ show_dictpath()#词典在哪里
 #可以通过搜狗词库添加词库
 #https://github.com/qinwf/cidian
 
-
 cn = "我想写一本书，名字叫做《R语言高效数据处理》。"   #构造中文文本
 tag_worker = worker(type = "tag")    #构造分词标注器
 tag_result = tagging(cn,tag_worker)   #进行分词标注
 tag_result    
-
 str(tag_result)  #查看数据类型
 #enframe(tag_result) -> tag_table  #转换数据存储格式
-
 vector_tag(segment(cn,wk),tag_worker)
 
